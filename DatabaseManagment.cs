2bf6802779c809d060d0b64cac02f67193a29662
Igor Korostelev <denisclassicv@gmail.com>
2023-08-07 17:27:58 +0300

    Добавьте файлы проекта.

---
+++ DatabaseManagment.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace Task_1
+{
+    public class DatabaseManagment
+    {
+        /// <summary>
+        /// Этот метод получает список с путями к файлам
+        /// Затем он создает БД и таблицу, если их еще нет
+        /// После уже импортирует все файлы в БД, разбивая строки по разделителям "||", поэтому их и нельзя удалить
+        /// </summary>
+        /// <param name="files"></param>
+        /// <returns></returns>
+        public async Task CreateAndImportDatabase(List<string> files)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    SqlCommand command = new SqlCommand();
+                    command.CommandText = "CREATE DATABASE filesdb";
+                    command.Connection = connection;
+                    await command.ExecuteNonQueryAsync();
+                    Console.WriteLine("База данных создана");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            try
+            {
+                string sqlExpression = @"CREATE TABLE Files 
+                                (Id INT PRIMARY KEY IDENTITY, 
+                                 Date NVARCHAR(50), 
+                                 LatinStr NVARCHAR(50),
+                                 RussianStr NVARCHAR(50),
+                                 Number BIGINT,
+                                 NumberFl DECIMAL(10,8))";
+
+                using (SqlConnection connection = new SqlConnection(connectionStringToDB))
+                {
+                    await connection.OpenAsync();
+                    SqlCommand command = new SqlCommand(sqlExpression, connection);
+                    await command.ExecuteNonQueryAsync();
+                    Console.WriteLine("Таблица Files создана");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionStringToDB))
+            {
+                await connection.OpenAsync();
+                int j = 1;
+                foreach (var file in files)
+                {
+                    using (var fs = File.OpenRead(file))
+                    {
+                        byte[] buffer = new byte[fs.Length];
+                        fs.Read(buffer, 0, buffer.Length);
+                        string textFromFile = Encoding.Default.GetString(buffer);
+                        _ = textFromFile.Replace("\n", "");
+                        _ = textFromFile.Remove(textFromFile.Length - 1);
+                        int i = 0;
+                        var arr = textFromFile.Split("||");
+
+                        while(i < arr.Length - 1)
+                        {
+                            SqlCommand command = new SqlCommand();
+                            command.Connection = connection;
+                            command.CommandText = @"INSERT INTO Files VALUES (@Date, @LatinStr, @RussianStr, @Number, @NumberFl)";
+                            command.Parameters.Add("@Date", SqlDbType.NVarChar);
+                            command.Parameters.Add("@LatinStr", SqlDbType.NVarChar);
+                            command.Parameters.Add("@RussianStr", SqlDbType.NVarChar);
+                            command.Parameters.Add("@Number", SqlDbType.Int);
+                            command.Parameters.Add("@NumberFl", SqlDbType.Decimal);
+
+                            command.Parameters["@Date"].Value = arr[i];
+                            command.Parameters["@LatinStr"].Value = arr[i + 1];
+                            command.Parameters["@RussianStr"].Value = arr[i + 2];
+                            command.Parameters["@Number"].Value = arr[i + 3];
+                            command.Parameters["@NumberFl"].Value = arr[i + 4];
+
+                            await command.ExecuteNonQueryAsync();
+
+                            i += 5;
+                            Console.WriteLine($"{j}/{50 - j}");
+                            j++;
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Здесь идет подсчет суммы всех целых чисел через sql-запрос
+        /// </summary>
+        /// <returns></returns>
+        public async Task GetSumm()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionStringToDB))
+            {
+                await connection.OpenAsync();
+                string sql = @"SELECT SUM(f.Number) FROM Files f";
+                SqlCommand command = new SqlCommand(sql, connection);
+                var summ = await command.ExecuteScalarAsync();
+                Console.WriteLine($"Сумма всех целых чисел равна {summ}.");
+            }
+        }
+
+        /// <summary>
+        /// Здесь считается медиана дробных чисел через sql-запрос
+        /// </summary>
+        /// <returns></returns>
+        public async Task GetMedian()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionStringToDB))
+            {
+                await connection.OpenAsync();
+                string sql = @"SELECT TOP (1) PERCENTILE_CONT (0.5)
+                                           WITHIN GROUP(ORDER BY f.NumberFl)
+                                           OVER()
+                                FROM Files f";
+                SqlCommand command = new SqlCommand(sql, connection);
+                var med = await command.ExecuteScalarAsync();
+                Console.WriteLine($"Медиана всех дробных чисел равна {med}.");
+            }
+        }
+
+        private static string connectionString = "Server=(localdb)\\mssqllocaldb;Database=master;Trusted_Connection=True;TrustServerCertificate=True;";
+        private static string connectionStringToDB = "Server=(localdb)\\mssqllocaldb;Database=filesdb;Trusted_Connection=True;TrustServerCertificate=True;";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,227 @@
+﻿
+using System.IO;
+using System.Text;
+
+namespace Task_1;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        ///Здесь создаются файлы и наполняются
+        ///Также создается класс для работы с БД
+        Console.WriteLine("Введите путь для создания файлов.");
+        string? pathFromUser = Console.ReadLine();
+        string pathTest = @"H:\B1 задание\MyTXT";
+        string pathDefault = @"D:\MyTXT";
+        string? path;
+        if (pathFromUser != "")
+        {
+            path = pathFromUser;
+        }
+        else
+        {
+            path = pathDefault;
+        }
+        Console.WriteLine("Создание 100 файлов...");
+        DateTime start = DateTime.Now.AddYears(-5);
+        Random random = new Random();
+        var files = new List<string>();
+        int range = (DateTime.Today - start).Days;
+        try
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                string name = path + i.ToString() + ".txt";
+                files.Add(name);
+                using (FileStream fs = File.Create(name))
+                { 
+                    for (int j = 0; j < 10; j++)
+                    {
+                        string input = RandomDate(start, random, range).ToShortDateString();
+                        input += "||" + RandomLatina() + "||" + RandomRussian() + "||" + RandomNumb() + "||" + RandomDouble() + "||" + "\n";
+                        byte[] info = new UTF8Encoding(true).GetBytes(input);
+                        fs.Write(info, 0, info.Length);
+                    }
+                    fs.Close();
+                }
+            }          
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        var db = new DatabaseManagment();
+
+        Console.WriteLine("Для объединения всех файлов в один введите 1\n" +
+            "Для импорта файлов в БД нажмите 2\n" +
+            "Для подсчета суммы всех целых чисел и медианы всех дробных чисел нажмите 3\n" +
+            "Для завершения программы введите 4");
+        ///Далее в цикле пункты 2-3-4 из задания можно повторять
+        ///Также создается БД если ее нет и туда ипортируются все файлы
+        while (true) 
+        {
+            string? choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    {
+                        Console.WriteLine("Если хотите удалить набор символов из всех файлов введите 1");
+                        var flag = Console.ReadLine();
+                        string? delete = "";
+                        if (flag == "1")
+                        {
+                            Console.WriteLine("Введите набор удаляемых символов, за исключением чисел и '||'...");
+                            delete += Console.ReadLine()?.ToString();
+                            while (delete == "||" || float.TryParse(delete, out _))
+                            {
+                                Console.WriteLine("Введите другой набор удаляемых символов, эти нельзя...");
+                                delete = Console.ReadLine()?.ToString();
+                            }
+                        }
+
+                        int countOfDeleteSubstrings = 0;
+                        using (var commonfile = File.Create(@"D:\CommonFile.txt"))
+                        {
+                            foreach (var file in files)
+                            {
+                                if (flag == "1" && delete != "")
+                                {
+                                    string textFromFile = "";
+                                    using (var fs = File.OpenRead(file))
+                                    {
+                                        byte[] buffer = new byte[fs.Length];
+                                        fs.Read(buffer, 0, buffer.Length);
+                                        textFromFile = Encoding.Default.GetString(buffer);
+                                        var position = textFromFile.IndexOf(delete);
+                                        while (position != -1)
+                                        {
+                                            textFromFile = textFromFile.Remove(position, delete.Length);
+                                            countOfDeleteSubstrings++;
+                                            position = textFromFile.IndexOf(delete);
+                                        }
+                                        fs.Close();
+                                    }
+                                    using (var fs = File.Create(file))
+                                    {
+                                        byte[] info = new UTF8Encoding(true).GetBytes(textFromFile);
+                                        fs.Write(info, 0, info.Length);
+                                        fs.CopyTo(commonfile);
+                                        fs.Close();
+                                    }
+                                    using (var fs = File.OpenRead(file))
+                                    {
+                                        fs.CopyTo(commonfile);
+                                        fs.Close();
+                                    }
+                                }
+                                else
+                                {
+                                    using (var fs = File.OpenRead(file))
+                                    {
+                                        fs.CopyTo(commonfile);
+                                        fs.Close();
+                                    }
+                                }
+                            }
+                            if (flag == "1")
+                            {
+                                Console.WriteLine($"Количество удаленных подстрок равно {countOfDeleteSubstrings}.");
+                            }
+
+                        }
+
+                        Console.WriteLine("Для объединения всех файлов в один введите 1\n" +
+                                            "Для импорта файлов в БД нажмите 2\n" +
+                                            "Для подсчета суммы всех целых чисел и медианы всех дробных чисел нажмите 3\n" +
+                                            "Для завершения программы введите 4");
+                    }
+                    break;
+
+                case "2":
+                    {
+                        await db.CreateAndImportDatabase(files);
+                        Console.WriteLine("Для объединения всех файлов в один введите 1\n" +
+                                            "Для импорта файлов в БД нажмите 2\n" +
+                                            "Для подсчета суммы всех целых чисел и медианы всех дробных чисел нажмите 3\n" +
+                                            "Для завершения программы введите 4");
+                    }
+                    break;
+
+                case "3":
+                    {
+                        await db.GetSumm();
+                        await db.GetMedian();
+                        Console.WriteLine("Для объединения всех файлов в один введите 1\n" +
+                                            "Для импорта файлов в БД нажмите 2\n" +
+                                            "Для подсчета суммы всех целых чисел и медианы всех дробных чисел нажмите 3\n" +
+                                            "Для завершения программы введите 4");
+                    }
+                    break;
+
+                case "4":
+                    {
+                        Environment.Exit(0);
+                    }
+                    break;
+
+                default:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+    }
+
+    // функции для работы с рандомными числами и датами для заполнения файлов
+    static DateTime RandomDate(DateTime start, Random gen, int range)
+    {
+        return start.AddDays(gen.Next(range));
+    }
+
+    static string RandomLatina()
+    {
+        Random rnd = new Random();
+        string latina = "";
+        for (int i = 0; i < 10; i++)
+        {
+            var position = rnd.Next(0, 52);
+            latina += LatinAlphabet[position];
+        }
+        return latina;
+    }
+
+    static string RandomRussian()
+    {
+        Random rnd = new Random();
+        string russian = "";
+        for (int i = 0; i < 10; i++)
+        {
+            var position = rnd.Next(0, 66);
+            russian += RussianAlphabet[position];
+        }
+        return russian;
+    }
+
+    static string RandomNumb()
+    {
+        Random rnd = new Random();
+        int numb;
+        do
+        {
+            numb = rnd.Next(0, 100000001);
+        } 
+        while (numb % 2 != 0);
+
+        return numb.ToString();
+    }
+
+    static string RandomDouble()
+    {
+        Random rnd = new Random();
+        var d = rnd.NextDouble();
+        return Math.Round(d * (20 - 1) + 1, 8).ToString();
+    }
+
+    private static string LatinAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";   //52
+    private static string RussianAlphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";   //66
+}
